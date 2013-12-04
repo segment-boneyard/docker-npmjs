04038bfeaf6e78638d64a3bece5429c13b94d14c
TJ Holowaychuk <tj@vision-media.ca>
2013-12-03 16:14:19 -0800

    add PORT

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo -e "[vhosts]\n$HOST:5984 = /registry/_design/app/_rewrite" >> /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
+echo -e "[vhosts]\n$HOST:$PORT = /registry/_design/app/_rewrite" >> /usr/local/etc/couchdb/local.d/npmjs-vhost.ini
 echo -e "127.0.0.1\t$HOST" >> /etc/hosts
-cat /opt/npmjs/kappa.json.default|sed -e "s/\${hostname}/$HOST/" > /opt/npmjs/kappa.json
+
+cat /opt/npmjs/kappa.json.default \
+  | sed -e "s/\$HOST/$HOST/" \
+  | sed -e "s/\$PORT/$PORT/" \
+  > /opt/npmjs/kappa.json
+
 couchdb -b; hapi -c /opt/npmjs/kappa.json & tail -f /usr/local/var/log/couchdb/couch.log
